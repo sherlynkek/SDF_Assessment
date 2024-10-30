a8448d163561adbb6c87d5c63b16751f969ad5d2
sherlynkek <sherlynkek00@gmail.com>
2024-10-30 13:46:43 +0800

    Task01

--- task01/src/vttp/batch5/sdf/task01/Main.java
+++ task01/src/vttp/batch5/sdf/task01/Main.java
@@ -66,5 +66,32 @@ public class Main {
             }
         }    
     }
+	
+	public class Season {
+    
+		// 1:spring, 2:summer, 3:fall, 4:winter
+	
+		public Season() {
+			
+			int season = 0;
+			if (season == 1) {
+				System.out.println("Spring");
+				
+			}
+			else if (season == 2) {
+				System.out.println("Summer");
+				
+			}
+			else if (season == 3) {
+				System.out.println("Fall");
+				
+			}
+			else if (season == 4) {
+				System.out.println("Winter");
+				
+			}
+	
+		}
+	}
 }
 

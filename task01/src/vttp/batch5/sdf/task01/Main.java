2e446a770a258b5b3a19652e2ec8dd816dbb818d
sherlynkek <sherlynkek00@gmail.com>
2024-10-30 12:04:46 +0800

    Task01

--- task01/src/vttp/batch5/sdf/task01/Main.java
+++ task01/src/vttp/batch5/sdf/task01/Main.java
@@ -8,18 +8,29 @@ import java.io.IOException;
 
 // Use this class as the entry point of your program
 
+// see if the file exists
+// read the file (day.csv)
+// split the data accordingly
+// compare every individual data to find the common season /and day /and month /and 
+// total number of cyclists
+// 
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 	
 		Main obj = new Main();
-		obj.run();
+		obj.file();
 
-		// System.out.printf("The " + position + "recorded number of cyclists was in " + season + ", on a " + day + "in the month of of " + month + "." + " There were a total of " + total + " cyclists. The weather was " + weather + "." + day + " was " + holiday);
+		// System.out.printf("The " + position 
+		// + "(position) recorded number of cyclists was in " + season 
+		// + "(season), on a " + day + "(day) in the month of " + month + "(month)." 
+		// + " There were a total of " + total + "(total) cyclists. The weather was " 
+		// + weather + "(weather)." + day + " (day) was " + holiday);
 
 	}
 
-	private void run() {
+	private void file() {
 		String csvFile = "C:\\Users\\Sherlyn Kek\\SDF\\SDF_Assessment\\task01\\day.csv";
         BufferedReader br = null;
         String line = "";
@@ -29,8 +40,11 @@ public class Main {
             {
                br = new BufferedReader(new FileReader(csvFile));
                while ((line = br.readLine()) != null) {
-				String[] id = line.split(cvsSplitBy);
-                System.out.println(" ");
+				String[] data = line.split(cvsSplitBy);
+                System.out.println("[season: " + data[0] + "] [mnth: " + data[1] 
+				+ "] [holiday: " + data[2] + "] [weekday: " + data[3] + "] [weathersit: " 
+				+ data[4] + "] [temp: " + data[5] + "] [hum: " + data[6] + "] [windspeed: " + data[7] 
+				+ "] [casual: " + data[8] + "] [registered: " + data[9] + "]");
             }
 
         } 
@@ -49,7 +63,26 @@ public class Main {
                 }
             }
         }    
-
-        System.out.println("#####################3");
     }
+
+	public class Season {
+		// 1:spring, 2:summer, 3:fall, 4:winter
+
+		if (season = 1) {
+			System.out.println("Spring");
+			
+		}
+		else if (season = 2) {
+			System.out.println("Summer");
+			
+		}
+		else if (season = 3) {
+			System.out.println("Fall");
+			
+		}
+		else if (season = 4) {
+			System.out.println("Winter");
+			
+		}
+	}
 }
